e707b0ecc154f96b6c89d466630006ddabdbb5c5
NesreenMassimi <42678185+NesreenMassimi@users.noreply.github.com>
2023-07-09 20:15:29 +0300

    creating the functions
    added increment. save, reset functions to the JS and added the CSS for whole project

---
+++ index.js
@@ -0,0 +1,39 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+let mem = 0
+let str = document.getElementById("display-log").innerText
+str += " "
+
+
+function increment() 
+{
+   count += 1 
+   console.log(count)
+   countEl.innerText = count
+}
+function save() {
+
+    mem = countEl.innerText
+    console.log(mem)
+    str += mem +" - "
+    document.getElementById("display-log").innerText = str
+    mem = 0 
+    countEl.innerText = 0
+    count =0
+   
+
+}
+
+
+
+
+/*function reset() { 
+                mem =0 
+                count = 0 
+                countEl.innerText = 0 
+                str = "Previous Entires :  " + mem +" - "
+                document.getElementById("display-log").innerText = str
+}
+*/
+
